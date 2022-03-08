f66bdaf0105cd59855bb73041faa413c0bf99339
Yann Klein <yann.klein@me.com>
2022-03-08 15:02:53 +0900

    added comments

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Jingle steps
-
 const STEPS = {
   longTimeAgo: 0, // from play to "Long Time Ago.." intro
   demoday: 4, // "Long time ago..." duration
@@ -9,14 +8,17 @@ for (const key in STEPS) {
   STEPS[key] = STEPS[key]*1000;
 }
 
+// HTML element selection
 const playBtn = document.querySelector('#play');
 const longTimeAgo = document.querySelector('#long-time-ago');
 const demoday = document.querySelector('h1');
 const rollText = document.querySelector('#text-roll');
 
+// Hide/show util functions
 const hide = el => el.classList.add('opacity-0', 'invisible');
 const show = el => el.classList.remove('opacity-0', 'invisible');
 
+// Steps action functions
 const showLongTimeAgo = () => {
   hide(playBtn);
   show(longTimeAgo);
@@ -26,11 +28,13 @@ const showDemoDay = () => {
   show(demoday);
 };
 
-const startJingle = (event) => {
+// Main jngle function
+const initJingle = (event) => {
   event.preventDefault();
   setTimeout(showLongTimeAgo, STEPS.longTimeAgo);
   setTimeout(showDemoDay, STEPS.demoday);
 
 }
 
-playBtn.addEventListener("click", startJingle);
+// Play button click listener
+playBtn.addEventListener("click", initJingle);
