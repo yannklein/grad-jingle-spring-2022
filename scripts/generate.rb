bea73c62b9a28268d7424a1b897914b98bc1abfa
Yann Klein <yann.klein@me.com>
2022-03-08 13:48:55 +0900

    erb base from project cal

---
+++ scripts/generate.rb
@@ -0,0 +1,11 @@
+require 'erb'
+require 'json'
+require 'ostruct'
+require 'date'
+
+def generate(data_path, template_path, output_path)
+  template = File.read(template_path)
+  data = OpenStruct.new(JSON.parse(File.read(data_path)))
+  generated = ERB.new(template).result(data.instance_eval { binding })
+  File.write(output_path, generated)
+end
