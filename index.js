5f668207086033ae3501622d3e8c09117ef1f3ce
Yann Klein <yann.klein@me.com>
2022-03-08 16:24:00 +0900

    finalize text animation

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Jingle steps
 const STEPS = {
   longTimeAgo: 0, // from play to "Long Time Ago.." intro
-  demoday: 4, // "Long time ago..." duration
-  sad: 6, // demo day
+  demoday: 4, // from "Long time ago..." to "Demo Day" duration
+  rollText: 10, // from demo day to Rolling text duration
+  closing: 30 //from text rollling to closing
 };
 for (const key in STEPS) {
   STEPS[key] = STEPS[key]*1000;
@@ -24,16 +25,28 @@ const showLongTimeAgo = () => {
   show(longTimeAgo);
 };
 const showDemoDay = () => {
-  hide(longTimeAgo);
   show(demoday);
+  demoday.style.transition = `font-size ${STEPS.rollText/2000}s ease-out`;
+  demoday.style.fontSize = '120px';
+  
 };
+const showRollingText = () => {
+  show(rollText);
+  rollText.style.transition = `transform ${STEPS.closing/1000}s ease-out, opacity 8s, visibility 8s`;
+  rollText.style.transform = `translate(-50%, -50%) perspective(130px) rotateX(20deg) translateY(-62%)`
+}
 
 // Main jngle function
 const initJingle = (event) => {
   event.preventDefault();
   setTimeout(showLongTimeAgo, STEPS.longTimeAgo);
-  setTimeout(showDemoDay, STEPS.demoday);
+  setTimeout(() => hide(longTimeAgo), STEPS.demoday - 1000)
 
+  setTimeout(showDemoDay, STEPS.demoday);
+  setTimeout(() => hide(demoday), STEPS.rollText)
+
+  setTimeout(showRollingText, STEPS.rollText);
+  setTimeout(() => hide(rollText), STEPS.closing)
 }
 
 // Play button click listener
--- scripts/generate.rb
+++ scripts/generate.rb
@@ -6,6 +6,8 @@ require 'date'
 def generate(data_path, template_path, output_path)
   template = File.read(template_path)
   data = OpenStruct.new(JSON.parse(File.read(data_path)))
+  data['text'].gsub!("\n", '<br/>')
+  data['title'].upcase!
   generated = ERB.new(template).result(data.instance_eval { binding })
   File.write(output_path, generated)
 end
